ac37b486431c1a3cb3b210f3d57e89fcebe23956
kangpeiqin <1060647301@qq.com>
2022-02-24 00:05:39 +0800

    update

--- index.js
+++ index.js
@@ -1,17 +1,38 @@
 const fs = require('fs');
-//读取文件
-// fs.readFile('./posts/about.md', 'utf8', (err, data) => {
-//     if (err) {
-//         console.error(err)
-//         return
-//     }
-//     console.log(data)
-// })
-// try {
-//     const data = fs.readFileSync('./posts/about.md', 'utf8')
-//     console.log(data)
-// } catch (err) {
-//     console.error(err)
-// }
-// const blogPaths = fs.readdirSync('./posts/');
-// console.log(blogPaths)
+const md = require('markdown-it')({
+    html: true,
+    linkify: true,
+    typographer: true
+});
+
+/**
+ * 读取文件
+ */
+const getFileData = (src) => {
+    return fs.readFileSync(src, 'utf8');
+}
+/**
+ * 往指定文件中写入内容
+ */
+const writeFile = (destFile, content) => {
+    fs.writeFileSync(destFile, content);
+}
+/**
+ * 解析 markdown 文件
+ */
+const parse = (content) => {
+    let contentArr = content.split('\n');
+    let postTitle = contentArr[1].slice(7).trim();
+    let date = contentArr[2].slice(6).trim();
+    let author = contentArr[3].slice(8).trim();
+    let mdBodyContent = contentArr.slice(5).join('\n');
+    return {
+        postTitle,
+        date,
+        author,
+        mdBodyContent
+    };
+}
+
+const data = parse(getFileData('./posts/about.md'));
+console.log(data)
